4845806c7945df6776f3e50b3c69b0cd68c20583
INDALARAJESH <109213968+INDALARAJESH@users.noreply.github.com>
2024-09-24 12:31:46 +0530

    Update main.tf

--- Jenkins-ubuntu/main.tf
+++ Jenkins-ubuntu/main.tf
@@ -44,6 +44,7 @@ resource "aws_instance" "jenkins_server" {
     # Install kubectl
     sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    sudo apt install gh -y
 
     # Clean up installation files
     rm -f awscliv2.zip
